f5c8c3a39b1cd4d92c7cdf9e716f9f109a2ed4ef
HBA <goddamncool@naver.com>
2025-02-21 13:47:18 +0900

    ✨ Add To do 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -9,10 +9,19 @@ function App() {
     },
   ]);
 
+  const inputRef = useRef(null);
+  const addTodo = () => {
+    const newTodo = {
+      id: Number(new Date()),
+      content: inputRef.current.value,
+    };
+    setTodo((prev) => [...prev, newTodo]);
+  };
+
   return (
     <>
-      <input />
-      <button>추가</button>
+      <input ref={inputRef} />
+      <button onClick={addTodo}>추가</button>
       <ul>
         {todo.map((el) => (
           <li key={el.id}>{el.content}</li>
